5c7f7dd03e1565500a0798e3e5afd492281538e4
Keshav Raj Choudhary <Keshavrajchaudhary@t-a-g.net>
2023-11-07 23:33:29 +0530

    added login modules with verification

---
+++ Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Admin_service.Models
+{
+    public class Admin
+    {
+        [Required]
+        public string Email { get; set; }
+        public string Role { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
